a4d89248d31c77e959590acc1ddc9d2e38e7ac92
NishanthSbz <nishanthsbz05@gmail.com>
2024-12-07 21:02:43 +0530

    our model base done

--- js/custom.js
+++ js/custom.js
@@ -44,3 +44,20 @@ $('.team_carousel').owlCarousel({
         }
     }
 })
+$(document).ready(function () {
+    $('.model_carousel').owlCarousel({
+      loop: true,
+      margin: 20,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 2500,
+      autoplayHoverPause: true,
+      center: true,
+      responsive: {
+        0: { items: 1 },
+        600: { items: 2 },
+        1000: { items: 3 }
+      }
+    });
+  });
+  
